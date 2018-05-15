340813be0d5d161fde7069b2ccba9c80aa1bd2e3
Thomas <tlattimore@users.noreply.github.com>
2018-05-15 05:59:57 -0400

    Fixing test to account for lack of duplicate pairs

--- TwoSum/TwoSum.test.js
+++ TwoSum/TwoSum.test.js
@@ -2,7 +2,7 @@ const TwoSum = require('./TwoSum.js');
 
 describe('some of two numbers', () => {
   let sampleData = [0, 1, 100, 99, 0, 10, 90, 30, 55, 33, 55, 75, 50, 51, 49, 50, 51, 49, 51];
-  const checker = [[1, 99], [100, 0 ], [10, 90 ], [51, 49], [50, 50],[51, 49]];
+  const checker = [[1, 99], [100, 0 ], [10, 90 ], [50, 50],[51, 49]];
   test('calculate and compare some of two numbers', () => {
     expect(TwoSum(sampleData, 100)).toEqual(checker);
   });
